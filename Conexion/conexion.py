5b4e4773368f0915b50b0dff00f0588b897ba5cf
JoelCuzme <mj.cuzmel@uea.edu.ec>
2026-03-12 12:31:24 -0500

    Corrigiendo conexion y eliminando secretos

--- Conexion/conexion.py
+++ Conexion/conexion.py
@@ -1,13 +1,15 @@
 import mysql.connector
 from mysql.connector import Error
+import os
 
 def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
-            host='localhost',
-            user='root',       # Usuario por defecto de phpMyAdmin
-            password='',       # Por defecto XAMPP no tiene contraseña
-            database='gestion_medica' # El nombre que le diste en phpMyAdmin
+            host=os.getenv('MYSQLHOST', 'mysql-3a27fee2-urssn90-b49f.f.aivencloud.com'),
+            user=os.getenv('MYSQLUSER', 'avnadmin'),
+            password=os.getenv('MYSQLPASSWORD'), # Se deja vacío para usar la variable de Render
+            database=os.getenv('MYSQLDATABASE', 'hospital'),
+            port=int(os.getenv('MYSQLPORT', 16398))
         )
         if conexion.is_connected():
             return conexion
